b2b3ded393053d509ac330cc50015f503c2b1c80
Elsa Gonsiorowski <gonsie@dimple.dyn.cs.rpi.edu>
2012-05-10 16:26:28 -0400

    looking at a bunch of peoples dotfiles repos

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 cd "$(dirname "$0")"
+
 git pull
+
 function doIt() {
     rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt
 else
