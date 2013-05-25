9812343bb244253d0bf614e3a64c5d227658cb21
Elsa Gonsiorowski (Andromeda) <gonsie@me.com>
2013-05-25 16:54:23 -0400

    added fish stuff, sync doesn't put it in the right spot though

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+alias emacs "/usr/local/Cellar/emacs/24.2/bin/emacs"
+alias em "emacs -nw"
+alias emw "emacs"
+
+# navigation
+alias marco "pwd"
+alias polo "dirs"
+alias cd.. "cd .."
+alias .. "cd .."
+alias ... "cd ../.."
+
+# shortcuts
+alias g "git"
+alias o "open"
+alias oo "open ."
+
+# http://defunkt.io/hub/
+alias git hub
+
+# enable aliases to be sudo'ed
+alias sudo 'sudo '
