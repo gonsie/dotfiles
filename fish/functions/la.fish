10d627542e6203f4ae24de17a43365254cfca94e
Elsa Gonsiorowski (Andromeda) <gonsie@me.com>
2013-05-28 17:51:27 -0400

    broke functions.fish into separate files

---
+++ fish/functions/la.fish
@@ -0,0 +1,4 @@
+
+function la --description 'List contents of directory, including hidden files in directory using long format'
+	ls -AFGhl $argv
+end
---
+++ fish/functions/md.fish
@@ -0,0 +1,4 @@
+function md -d "Create and enter a directory (unsofisticated)"
+         mkdir $argv
+         cd $argv
+end
