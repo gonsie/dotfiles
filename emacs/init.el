bfcffe5a80b336e00e006e3f587828148967b6b0
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2017-10-20 15:42:14 -0700

    i want my custom-set-faces everywhere

--- emacs/init.el
+++ emacs/init.el
@@ -235,12 +235,14 @@ fonts."
     "%-" ;; fill with '-'
 ))
 
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(neo-dir-link-face ((t (:foreground "yellow"))))
-;;  '(neo-file-link-face ((t (:foreground "color-255"))))
-;;  '(org-document-info ((t (:foreground "blue"))))
-;;  '(org-document-title ((t (:foreground "blue" :weight bold)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(neo-dir-link-face ((t (:foreground "yellow"))))
+ '(neo-file-link-face ((t (:foreground "color-255"))))
+ '(org-document-info ((t (:foreground "blue"))))
+ '(org-document-title ((t (:foreground "blue" :weight bold))))
+ '(org-table ((t (:forground "blue"))))
+ '(widget-field ((t (:background "navy")))))
