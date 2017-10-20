562c0c1349728eb0693d3c283fee77979f2df6df
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2017-10-20 15:39:28 -0700

    org agenda keybindings

--- emacs/init.el
+++ emacs/init.el
@@ -83,6 +83,12 @@
 
 ;; ORG
 
+;; keybindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (require 'ox-md)
 
 ;; active Babel languages
