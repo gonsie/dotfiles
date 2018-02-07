3472e1e122bd05970ab5feb808753607baf0b592
Elsa Gonsiorowski (LOFT) <gonsie@llnl.gov>
2018-02-07 11:45:32 -0500

    org comes from a new place

--- emacs/init.el
+++ emacs/init.el
@@ -8,6 +8,7 @@
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;; Install by hand:
