df15c71e69318665fff27ee51b328c193dac14fa
Elsa Gonsiorowski (LOFT) <gonsie@llnl.gov>
2018-02-07 11:50:49 -0500

    org template

--- emacs/init.el
+++ emacs/init.el
@@ -114,6 +114,7 @@
      (add-to-list 'org-structure-template-alist '("n" "#+NAME: "))
      (add-to-list 'org-structure-template-alist '("p" ":PROPERTIES:\n?\n:END:"))
      (add-to-list 'org-structure-template-alist '("t" "#+title: ?\n#+author: Elsa Gonsiorowski\n#+date: \n"))
+     (add-to-list 'org-structure-template-alist '("T" "#+title: ?\n#+author: Elsa Gonsiorowski\n#+date: \n\n#+property: exported: nil\n#+options:\n"))
      (add-to-list 'org-structure-template-alist '("E" "#+END_EXAMPLE\n?\n#+BEGIN_EXAMPLE"))
      ))
 
