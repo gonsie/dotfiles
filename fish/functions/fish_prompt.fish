f81f50e44e405add866044030534551cd5e7ee29
Elsa Gonsiorowski <gonsiorowski1@llnl.gov>
2018-06-18 11:21:31 -0500

    fish prompt with venv

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -6,8 +6,8 @@ function fish_prompt --description 'Write out the prompt'
 		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
 	end
 
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
+	if not set -q __fish_prompt_color_normal
+		set -g __fish_prompt_color_normal (set_color normal)
 	end
 
 	if not set -q -g __fish_classic_git_functions_defined
@@ -41,44 +41,51 @@ function fish_prompt --description 'Write out the prompt'
 
 	case root
 
-		if not set -q __fish_prompt_cwd
+		if not set -q __fish_prompt_color_cwd
 			if set -q fish_color_cwd_root
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+				set -g __fish_prompt_color_cwd (set_color $fish_color_cwd_root)
 			else
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+				set -g __fish_prompt_color_cwd (set_color $fish_color_cwd)
 			end
 		end
 
 	case '*'
 
-		if not set -q __fish_prompt_cwd
-			set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+		if not set -q __fish_prompt_color_cwd
+			set -g __fish_prompt_color_cwd OD(set_color $fish_color_cwd)
 		end
 
 	end
 
 	set -l prompt_status
 	if test $last_status -ne 0
-		if not set -q __fish_prompt_status
-			set -g __fish_prompt_status (set_color $fish_color_status)
+		if not set -q __fish_prompt_color_status
+			set -g __fish_prompt_color_status (set_color $fish_color_status)
 		end
-		set prompt_status "$__fish_prompt_status [$last_status]$__fish_prompt_normal"
+		set prompt_status "$__fish_prompt_color_status [$last_status]$__fish_prompt_color_normal"
 	end
 
-	if not set -q __fish_prompt_user
-		set -g __fish_prompt_user (set_color $fish_color_user)
+	if not set -q __fish_prompt_color_user
+		set -g __fish_prompt_color_user (set_color $fish_color_user)
 	end
-	if not set -q __fish_prompt_host
-		set -g __fish_prompt_host (set_color $fish_color_host)
+	if not set -q __fish_prompt_color_host
+		set -g __fish_prompt_color_host (set_color $fish_color_host)
 	end
 
-        if not set -q __fish_prompt_git
-                set -g __fish_prompt_git (set_color green)
+        if not set -q __fish_prompt_color_git
+                set -g __fish_prompt_color_git (set_color green)
         end
 
-        if not set -q __fish_prompt_time
-                set -g __fish_prompt_time (set_color ffaf5f)
+        if not set -q __fish_prompt_color_time
+                set -g __fish_prompt_color_time (set_color ffaf5f)
         end
 
-	echo -n -s "$__fish_prompt_time" (date +%T) " $__fish_prompt_user" "$USER" "$__fish_prompt_normal" @ "$__fish_prompt_host" "$__fish_prompt_hostname" "$__fish_prompt_normal" ' ' "$__fish_prompt_cwd" (basename (prompt_pwd)) "$__fish_prompt_git" (__fish_git_prompt) "$__fish_prompt_normal" "$prompt_status" "$delim" ' '
+        if set -q VIRTUAL_ENV
+                if not set -q __fish_prompt_color_venv
+                        set -g __fish_prompt_color_venv (set_color -b blue white)
+                end
+                set prompt_venv " $__fish_prompt_color_venv("(basename "$VIRTUAL_ENV")")$__fish_prompt_color_normal"
+        end
+
+	echo -n -s "$__fish_prompt_color_time" (date +%T) " $__fish_prompt_color_user" "$USER" "$__fish_prompt_color_normal" @ "$__fish_prompt_color_host" "$__fish_prompt_hostname" "$__fish_prompt_color_normal" ' ' "$__fish_prompt_color_cwd" (basename (prompt_pwd)) "$__fish_prompt_color_git" (__fish_git_prompt) "$__fish_prompt_color_normal" "$prompt_venv" "$prompt_status" "$delim" ' '
 end
