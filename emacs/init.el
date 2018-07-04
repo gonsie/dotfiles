feb9a1ac8ba4e65d599c07592917a573c758e0a0
Elsa Gonsiorowski (LOFT) <gonsiorowski1@llnl.gov>
2018-07-04 17:49:41 +0200

    basic package setup https://huytd.github.io/emacs-from-scratch.html

--- emacs/init.el
+++ emacs/init.el
@@ -1,18 +1,20 @@
 ;; 1. Connect to MELPA
 
+;; Package configs
 (require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
-  (add-to-list 'package-archives (cons "melpa" url) t))
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(setq package-enable-at-startup nil)
+(setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
 ;; Install by hand:
-;; - use-package
 ;; - org
 
 ;; PACKAGES
