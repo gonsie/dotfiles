8d3b92248b751e699032972adf0367dbdc85cef0
Elsa Gonsiorowski (Uranus) <gonsiorowski1@llnl.gov>
2018-10-19 11:57:17 -0700

    whitespace

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,73 +3,73 @@ function fish_prompt --description 'Write out the prompt'
 
     # Just calculate these once, to save a few cycles when displaying the prompt
     if not set -q __fish_prompt_hostname
-	set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+        set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
     end
 
     if not set -q __fish_prompt_color_normal
-	set -g __fish_prompt_color_normal (set_color normal)
+        set -g __fish_prompt_color_normal (set_color normal)
     end
 
     if not set -q -g __fish_classic_git_functions_defined
-	set -g __fish_classic_git_functions_defined
+        set -g __fish_classic_git_functions_defined
 
-	function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-	    if status --is-interactive
-		set -e __fish_prompt_user
-		commandline -f repaint ^/dev/null
-	    end
-	end
+        function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
+            if status --is-interactive
+                set -e __fish_prompt_user
+                commandline -f repaint ^/dev/null
+            end
+        end
 
-	function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-	    if status --is-interactive
-		set -e __fish_prompt_host
-		commandline -f repaint ^/dev/null
-	    end
-	end
+        function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
+            if status --is-interactive
+                set -e __fish_prompt_host
+                commandline -f repaint ^/dev/null
+            end
+        end
 
-	function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-	    if status --is-interactive
-		set -e __fish_prompt_status
-		commandline -f repaint ^/dev/null
-	    end
-	end
+        function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
+            if status --is-interactive
+                set -e __fish_prompt_status
+                commandline -f repaint ^/dev/null
+            end
+        end
     end
 
     set -l delim ' >'
 
     switch $USER
 
-	case root
+        case root
 
-	    if not set -q __fish_prompt_color_cwd
-		if set -q fish_color_cwd_root
-		    set -g __fish_prompt_color_cwd (set_color $fish_color_cwd_root)
-		else
-		    set -g __fish_prompt_color_cwd (set_color $fish_color_cwd)
-		end
-	    end
+            if not set -q __fish_prompt_color_cwd
+                if set -q fish_color_cwd_root
+                    set -g __fish_prompt_color_cwd (set_color $fish_color_cwd_root)
+                else
+                    set -g __fish_prompt_color_cwd (set_color $fish_color_cwd)
+                end
+            end
 
-	case '*'
+        case '*'
 
-	    if not set -q __fish_prompt_color_cwd
-		set -g __fish_prompt_color_cwd (set_color $fish_color_cwd)
-	    end
+            if not set -q __fish_prompt_color_cwd
+                set -g __fish_prompt_color_cwd (set_color $fish_color_cwd)
+            end
 
     end
 
     set -l prompt_status
     if test $last_status -ne 0
-	if not set -q __fish_prompt_color_status
-	    set -g __fish_prompt_color_status (set_color $fish_color_status)
-	end
-	set prompt_status "$__fish_prompt_color_status [$last_status]$__fish_prompt_color_normal"
+        if not set -q __fish_prompt_color_status
+            set -g __fish_prompt_color_status (set_color $fish_color_status)
+        end
+        set prompt_status "$__fish_prompt_color_status [$last_status]$__fish_prompt_color_normal"
     end
 
     if not set -q __fish_prompt_color_user
-	set -g __fish_prompt_color_user (set_color $fish_color_user)
+        set -g __fish_prompt_color_user (set_color $fish_color_user)
     end
     if not set -q __fish_prompt_color_host
-	set -g __fish_prompt_color_host (set_color $fish_color_host)
+        set -g __fish_prompt_color_host (set_color $fish_color_host)
     end
 
     set -g __fish_git_prompt_showcolorhints 1
