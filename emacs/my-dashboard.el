51654fb6d2f2c2a29e9fb8f8ffb9031fcc71aaa1
Elsa Gonsiorowski (titania) <gonsiorowski1@llnl.gov>
2022-03-31 21:59:14 -0400

    working on dashboard
    2 col view with functions and extra space

--- emacs/my-dashboard.el
+++ emacs/my-dashboard.el
@@ -25,26 +25,57 @@
   "Switch to the dashboard buffer"
   (interactive)
   (switch-to-buffer "*dashboard*"))
-(global-set-key (kbd "C-c d") #'my/switch-to-dashboard)
+;;(global-set-key (kbd "C-c d") #'my/switch-to-dashboard)
 
+;;; TODO:
+;;; Use follow-mode to get a 2 column view of the dashboard
+;;; 1. make the 2 column / 2 window view appear when switching into the dashboard
+;;; 2. make the 2 window view disappear when leaving the dashboard
+;;;    a. via 'q'
+;;;    b. by selecting a file
+;;; 3. make the projects group start "in the next column" aka add some blank space
 
-;; Insert Project list
+(defun my/display-2col-dashboard ()
+  "Use follow-mode to display a 2 column / 2 window dashboard"
+  (interactive)
+  (switch-to-buffer "*dashboard*")
+  (follow-mode 1)
+  (split-window-right))
+(global-set-key (kbd "C-c d") #'my/display-2col-dashboard)
 
+(defun my/dashboard-quit-window ()
+  "Close all extra dashboard windows, then quit dashboard"
+  (interactive)
+  (let* ((buf-list (get-buffer-window-list "*dashboard*"))
+         (cur-buff (car buf-list))
+         (othr-buf (cdr buf-list)))
+    (mapcar (lambda (window)
+              (delete-window window))
+            othr-buf))
+  (quit-window))
+(define-key dashboard-mode-map (kbd "q") #'my/dashboard-quit-window)
+
+;; widget creation helper
+(defun dashboard-widget-create-existing-file (path name)
+  "Helper function to create a widget for existing files"
+  (insert "\n    ")
+  (widget-create 'item
+                 :tag name
+                 :action `(lambda (&rest ignore) (my/dashboard-quit-window)(find-file-existing ,path))
+                 :mouse-face 'highlight
+                 :follow-link "\C-m"
+                 :button-prefix ""
+                 :button-suffix ""
+                 :format "%[%t%]"))
+
+;; Project List Section
 (defun dashboard-insert-project-list (list-display-name list)
   "Render LIST-DISPLAY-NAME title and items of LIST."
   (when (car list)
     (dashboard-insert-heading list-display-name)
     (mapc (lambda (el)
             (setq el (concat "~/Projects/" (car el)))
-            (insert "\n    ")
-            (widget-create 'item
-                           :tag (abbreviate-file-name el)
-                           :action `(lambda (&rest ignore) (find-file-existing ,el))
-                           :mouse-face 'highlight
-                           :follow-link "\C-m"
-                           :button-prefix ""
-                           :button-suffix ""
-                           :format "%[%t%]"))
+            (dashboard-widget-create-existing-file el (abbreviate-file-name el)))
           list)))
 
 (defun dashboard-insert-projects (list-size)
@@ -63,21 +94,14 @@
 (add-to-list 'dashboard-item-generators  '(projects . dashboard-insert-projects))
 (add-to-list 'dashboard-items '(projects . 10) t)
 
-;; helper function
-(defun dashboard-widget-create-existing-file (path name)
-  "Helper function to create a widget for existing files"
-  (insert "\n    ")
-  (widget-create 'item
-                 :tag name
-                 :action `(lambda (&rest ignore) (find-file-existing ,path))
-                 :mouse-face 'highlight
-                 :follow-link "\C-m"
-                 :button-prefix ""
-                 :button-suffix ""
-                 :format "%[%t%]"))
+;; Frequents Section
 
 (defun dashboard-insert-freqs (list-size)
   (dashboard-insert-heading "Frequent[s]:")
+  ;; TODO: machine-specific freq items
+  (mapcar '(lambda (f)
+             (dashboard-widget-create-existing-file (car f)(car (cdr f))))
+          my/dashboard-freqs)
   (insert "\n    ")
   (widget-create 'item
                  :tag "*eshell*"
@@ -108,7 +132,17 @@
 (add-to-list 'dashboard-item-generators  '(freqs . dashboard-insert-freqs))
 (add-to-list 'dashboard-items '(freqs) t)
 
+;; Extra Space Section
+(defun dashboard-generate-extraspace (list-size)
+  "Add some extra newlines. Used to improve the 2 column view."
+  (dotimes (i list-size)
+           (insert "\n")))
+
+(add-to-list 'dashboard-item-generators '(extraspace . dashboard-generate-extraspace))
+(add-to-list 'dashboard-items '(extraspace) t)
+
 ;; Define items to show
-(setq dashboard-items '((recents  . 5)
+(setq dashboard-items '((recents . 5)
                         (freqs . 1)
+                        (extraspace . 6)
                         (projects . 20)))
